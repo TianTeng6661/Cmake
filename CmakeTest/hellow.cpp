81610c952647b606b188f638d5e1de2e3202b20e
tianteng <tianteng@zhihu.com>
2021-06-10 17:34:08 +0800

    first commit

---
+++ CmakeTest/hellow.cpp
@@ -0,0 +1,8 @@
+//
+//  hellow.cpp
+//  CmakeTest
+//
+//  Created by TianTeng on 2021/5/31.
+//
+
+#include "hellow.hpp"
---
+++ CmakeTest/hellow.hpp
@@ -0,0 +1,13 @@
+//
+//  hellow.hpp
+//  CmakeTest
+//
+//  Created by TianTeng on 2021/5/31.
+//
+
+#ifndef hellow_hpp
+#define hellow_hpp
+
+#include <stdio.h>
+
+#endif /* hellow_hpp */
